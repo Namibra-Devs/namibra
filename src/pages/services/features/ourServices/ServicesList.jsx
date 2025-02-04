f11010d3e152d74dc6de468e7fb4cb7fbbe26de4
Tyler Gobka Bright <tgbright88@gmail.com>
2025-02-04 17:10:35 +0000

    Added functionality to view All services at the services page

---
+++ src/pages/services/features/ourServices/ServicesList.jsx
@@ -0,0 +1,34 @@
+import ButtonFilled from "../../../../components/buttons/ButtonFilled";
+const ServicesList = ({ services, showViewMore }) => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 mb-10">
+        {services.map((service, index) => (
+          <div
+            key={index}
+            className="bg-white shadow-custom-light group hover:shadow-inner border transition-all duration-700 ease rounded-2xl overflow-hidden"
+          >
+            <img
+              src={service.image}
+              alt={service.title}
+              className="w-full min-h-32 object-cover group-hover:brightness-75 duration-700"
+            />
+            <div className="p-5 text-left">
+              <h2 className="text-lg font-semibold mb-2">{service.title}</h2>
+              <p className="text-gray-600">{service.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* "View More" button (Only show when needed) */}
+      {showViewMore && (
+        <div className=" flex justify-center mt-4 text-center">
+          <ButtonFilled label="Learn More" link="/services" />
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default ServicesList;
