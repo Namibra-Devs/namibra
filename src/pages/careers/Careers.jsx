12948acfc2d55c43867fa4b9368629255e83a07d
Tyler Gobka Bright <tgbright88@gmail.com>
2025-02-05 06:41:14 +0000

    Added Positions Section

--- src/pages/careers/Careers.jsx
+++ src/pages/careers/Careers.jsx
@@ -1,16 +1,17 @@
-
-import CareersHero from '../../components/heroSections/CareersHero'
-import CareersIntro from './features/CareersIntro'
-import NamibraCulture from './features/namibraCulture/NamibraCulture'
+import CareersHero from "../../components/heroSections/CareersHero";
+import CareersIntro from "./features/CareersIntro";
+import NamibraCulture from "./features/namibraCulture/NamibraCulture";
+import OpenPositions from "./features/openPositions/OpenPositions";
 
 const Careers = () => {
   return (
     <>
-        <CareersHero/>
-        <CareersIntro/>
-        <NamibraCulture/>
+      <CareersHero />
+      <CareersIntro />
+      <NamibraCulture />
+      <OpenPositions />
     </>
-  )
-}
+  );
+};
 
-export default Careers
+export default Careers;
