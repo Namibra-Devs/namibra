746c01b0622be05e5789f48a515d1fddc766dfdd
Tyler Gobka Bright <tgbright88@gmail.com>
2025-02-06 14:47:42 +0000

    Modified Display of Portfolio Projects In the Home Page to display first two projects

--- src/pages/home/features/ourPortfolio/OurPortfolioProjects.jsx
+++ src/pages/home/features/ourPortfolio/OurPortfolioProjects.jsx
@@ -1,8 +1,8 @@
 import Shape from "../../../../assets/icons/shape.png";
 import ArrowC from "../../../../assets/icons/Arrow.png";
+import SelectedPortfolio from "./SelectedPortfolio";
 // import ProjectOne from "../../../../assets/our-portfolio/projectone.png";
-import ButtonFilled from "../../../../components/buttons/ButtonFilled";
-import portfolioData from "../../../ourPortfolio/features/portfolioData";
+
 
 const OurPortfolioProjects = () => {
 
@@ -26,21 +26,7 @@ const OurPortfolioProjects = () => {
             our projects.
           </h3>
         </div>
-        <div className="flex flex-col gap-10">
-          {portfolioData[0].projects.slice(0, 2).map((project) => (
-            <div key={project.id}>
-              <img src={project.image} alt={project.title} className="w-full mb-10" />
-              <div className="space-y-2">
-                <h3 className="text-xl font-semibold">{project.title}</h3>
-                <p className="text-gray-600">{project.description}</p>
-              </div>
-            </div>
-          ))}
-    
-          <div className="text-center mt-10">
-            <ButtonFilled label="View More"link="/portfolio" />
-          </div>
-        </div>
+        <SelectedPortfolio/>
       </div>
     </section>
   );
